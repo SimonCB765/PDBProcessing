171117cefa2b854f23f0706cb654cc85f14410c3
unknown <SimonCB765@gmail.com>
2014-01-30 21:47:43 +0000

    Removed the drift calculation

--- processPSIoutput.py
+++ processPSIoutput.py
@@ -13,48 +13,36 @@ def main(PSIoutput):
     """
 
     currentQuery = ''
-    driftFound = False
-    hitsFoundLastIteration = {}
-    hitsFoundThisIteration = {}
+    hitsFound = {}
     similaritiesFound = {}
 
     BLASTOutput = open(PSIoutput, 'r')
     for line in BLASTOutput:
         chunks = line.split()
         if len(chunks) == 0:
-            # If the line is a blank line, then the end of a query has been reached.
-            driftFound = False
-            for hit in hitsFoundThisIteration:
-                if hit != currentQuery:
-                    # Only record the similarity if the query and hit are not the same
-                    pair = tuple(sorted([currentQuery, hit]))
-                    if pair in similaritiesFound and similaritiesFound[pair]['Identity'] < hitsFoundThisIteration[hit]['Identity']:
-                        # If the pair exists and the recorded similarity is less than the newly found one, then update the record.
-                        similaritiesFound[pair]['Identity'] = hitsFoundThisIteration[hit]['Identity']
-                        similaritiesFound[pair]['MatchLength'] = hitsFoundThisIteration[hit]['MatchLength']
-                    else:
-                        # If the pair does not exist, then record the information about it.
-                        similaritiesFound[pair] = {'Identity' : hitsFoundThisIteration[hit]['Identity'], 'MatchLength' : hitsFoundThisIteration[hit]['MatchLength']}
-            hitsFoundLastIteration = {}
-            hitsFoundThisIteration = {}
-        elif driftFound:
             continue
         elif chunks[0] == '#' and chunks[1] == 'Query:':
             # If the current line is a comment line with the query protein information on it.
-            for key in hitsFoundLastIteration:
-                # Compare currentRoundHits with previousRoundHits to check for drift.
-                if not key in hitsFoundThisIteration:
-                    # If a hit is recorded in hitsFoundLastIteration but not in hitsFoundThisIteration then drift is deemed to have occurred.
-                    hitsFoundThisIteration = hitsFoundLastIteration
-                    driftFound = True
-            if not driftFound:
-                currentQuery = chunks[2]
-                hitsFoundLastIteration = hitsFoundThisIteration
-                hitsFoundThisIteration = {}
+            nextQuery = chunks[2]
+            if currentQuery != nextQuery:
+                # A new query has been found. Write out the hits from the last query.
+                for hit in hitsFound:
+                    if hit != currentQuery:
+                        # Only record the similarity if the query and hit are not the same
+                        pair = tuple(sorted([currentQuery, hit]))
+                        if pair in similaritiesFound and similaritiesFound[pair]['Identity'] < hitsFound[hit]['Identity']:
+                            # If the pair exists and the recorded similarity is less than the newly found one, then update the record.
+                            similaritiesFound[pair]['Identity'] = hitsFound[hit]['Identity']
+                            similaritiesFound[pair]['MatchLength'] = hitsFound[hit]['MatchLength']
+                        else:
+                            # If the pair does not exist, then record the information about it.
+                            similaritiesFound[pair] = {'Identity' : hitsFound[hit]['Identity'], 'MatchLength' : hitsFound[hit]['MatchLength']}
+            currentQuery = nextQuery
+            hitsFound = {}
         elif chunks[0] == currentQuery:
             # An alignment is recorded on the line if the line starts with the query protein.
             hit = chunks[1]
-            hitsFoundThisIteration[hit] = {'Identity' : chunks[2], 'MatchLength' : chunks[3]}
+            hitsFound[hit] = {'Identity' : chunks[2], 'MatchLength' : chunks[3]}
     BLASTOutput.close()
 
     return similaritiesFound
