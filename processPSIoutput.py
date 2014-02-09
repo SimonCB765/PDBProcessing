aa09b365ec882c1609804ccf76e872c9b12da022
unknown <SimonCB765@gmail.com>
2014-02-09 11:19:58 +0000

    Added ability to process within a set range of PDBIDs

--- processPSIoutput.py
+++ processPSIoutput.py
@@ -46,3 +46,65 @@ def main(PSIoutput):
     BLASTOutput.close()
 
     return similaritiesFound
+
+
+def processWithLimits(PSIoutput):
+    """Extracts the relevant information from the PSI-BLAST output.
+
+    @param PSIoutput: The location of the file containing the PSI-BLAST results.
+    @type PSIoutput:  string
+
+    """
+
+    currentQuery = ''
+    hitsFound = {}
+    similaritiesFound = {}
+
+    minPDBID = '00'
+    maxPDBID = 'ZZ'
+    queryInRange = False
+    queryAboveRange = False
+
+    BLASTOutput = open(PSIoutput, 'r')
+    for line in BLASTOutput:
+        chunks = line.split()
+        if len(chunks) == 0:
+            continue
+        elif chunks[0] == '#' and chunks[1] == 'Query:':
+            # If the current line is a comment line with the query protein information on it.
+            nextQuery = chunks[2]
+            if currentQuery != nextQuery:
+                # A new query has been found. Write out the hits from the last query.
+                for hit in hitsFound:
+                    if hit != currentQuery:
+                        # Only record the similarity if the query and hit are not the same
+                        pair = tuple(sorted([currentQuery, hit]))
+                        if pair in similaritiesFound and similaritiesFound[pair]['Identity'] < hitsFound[hit]['Identity']:
+                            # If the pair exists and the recorded similarity is less than the newly found one, then update the record.
+                            similaritiesFound[pair]['Identity'] = hitsFound[hit]['Identity']
+                            similaritiesFound[pair]['MatchLength'] = hitsFound[hit]['MatchLength']
+                        else:
+                            # If the pair does not exist, then record the information about it.
+                            similaritiesFound[pair] = {'Identity' : hitsFound[hit]['Identity'], 'MatchLength' : hitsFound[hit]['MatchLength']}
+            if nextQuery[1:3] >= minPDBID and nextQuery[1:3] <= maxPDBID:
+                queryInRange = True
+            else:
+                queryInRange = False
+            if nextQuery[1:3] > maxPDBID:
+                queryAboveRange = True
+            else:
+                queryAboveRange = False
+            currentQuery = nextQuery
+            hitsFound = {}
+        elif chunks[0] == currentQuery:
+            # An alignment is recorded on the line if the line starts with the query protein.
+            hit = chunks[1]
+            if queryInRange and hit[1:3] >= minPDBID:
+                # If the query is in the range of interest and the hit is for a pdb ID ordered within or above the range
+                hitsFound[hit] = {'Identity' : chunks[2], 'MatchLength' : chunks[3]}
+            elif queryAboveRange and (hit[1:3] >= minPDBID and hit[1:3] <= maxPDBID):
+                # If the query is above the range and the hit is in the range
+                hitsFound[hit] = {'Identity' : chunks[2], 'MatchLength' : chunks[3]}
+    BLASTOutput.close()
+
+    return similaritiesFound
